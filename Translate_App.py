008c4476f89e880b2a05e8bf36c159ac8228e895
Süleyman Çıracı <man.ciraci@gmail.com>
2025-09-19 03:33:26 +0300

    Initial commit without secrets

--- Translate_App.py
+++ Translate_App.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.messages import HumanMessage, SystemMessage
@@ -38,3 +39,45 @@ add_routes(
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
+=======
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from fastapi import FastAPI
+from langserve import add_routes
+
+
+# Ortam değişkenini yükle (.env içinde GOOGLE_API_KEY olmalı)
+load_dotenv()
+
+# Gemini modelini başlat
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0.1)
+
+system_prompt="Translate the following from English to {language}"
+prompt_template=ChatPromptTemplate.from_messages(
+    [
+        ("system",system_prompt), ("user", "{text}")
+    ]
+)
+
+parser=StrOutputParser()
+chain=prompt_template | model | parser
+
+app=FastAPI(
+    title="Translator App!",
+    version="1.0.0",
+    description="Translation Chat Bot",
+)
+
+add_routes(
+    app,
+    chain,
+    path="/chain"
+)
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+>>>>>>> 1728ea1 (Initial commit without secrets)
